9675e2de5ff41f1bd89943baf3d8e99b6e381177
DewMan001 <deweymanster@gmail.com>
2016-08-19 15:10:49 +0800

    Some basic design done on the main form

--- OOPAssess1-BingoOfficeSupplies/Form1.Designer.cs
+++ OOPAssess1-BingoOfficeSupplies/Form1.Designer.cs
@@ -28,21 +28,71 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.lBox_ProductsDisplay = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // lBox_ProductsDisplay
+            // 
+            this.lBox_ProductsDisplay.FormattingEnabled = true;
+            this.lBox_ProductsDisplay.ItemHeight = 20;
+            this.lBox_ProductsDisplay.Location = new System.Drawing.Point(12, 95);
+            this.lBox_ProductsDisplay.Name = "lBox_ProductsDisplay";
+            this.lBox_ProductsDisplay.Size = new System.Drawing.Size(148, 264);
+            this.lBox_ProductsDisplay.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 72);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(126, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Select a Product";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(198, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Information";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(469, 72);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 20);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Picture";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(278, 244);
+            this.ClientSize = new System.Drawing.Size(648, 371);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lBox_ProductsDisplay);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox lBox_ProductsDisplay;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
 
